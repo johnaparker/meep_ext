fbdbf401d91d8f8b447328450669945a589b47fe
John <japarker@uchicago.edu>
2018-03-21 01:44:49 -0500

    added 3d force box; other 2d monitors

--- meep_ext/__init__.py
+++ meep_ext/__init__.py
@@ -1,5 +1,6 @@
-from .monitor import (add_force_box_2d, add_flux_plane, add_flux_box,
+from .monitor import (add_force_box_2d, add_force_box, add_flux_plane, add_flux_box,
                     freq_data)
+
 from .material import (drude_lorentz_material, lorentz_material,
                     fit_drude_lorentz, fit_lorentz_at_single_wavelength,
                     get_eps)
--- meep_ext/monitor.py
+++ meep_ext/monitor.py
@@ -6,25 +6,38 @@ def freq_data(fmin, fmax):
     df = abs(fmax - fmin)
     return fcen, df
 
-def force_box_2d(position, size, component):
-    """create 4 force planes (2D) to calculate the component of a force"""
-    force_x1 = meep.ForceRegion(center=position - meep.Vector3(0,size[1]/2),
-                            size=meep.Vector3(size[0], 0), 
-                            direction=component, weight=-1)
-    force_x2 = meep.ForceRegion(center=position + meep.Vector3(0,size[1]/2),
-                            size=meep.Vector3(size[0], 0), 
-                            direction=component, weight=1)
+def force_box(position, size, component):
+    """create 6 force planes to calculate the component of a force"""
+    position = meep.Vector3(*position)
+    size = meep.Vector3(*size)
 
-    force_y1 = meep.ForceRegion(center=position - meep.Vector3(size[0]/2, 0),
-                            size=meep.Vector3(0, size[1]), 
-                            direction=component, weight=-1)
-    force_y2 = meep.ForceRegion(center=position + meep.Vector3(size[0]/2, 0),
-                            size=meep.Vector3(0, size[1]), 
-                            direction=component, weight=1)
+    force_x1 = meep.ForceRegion(center=position - meep.Vector3(size[0]/2, 0, 0),
+                            size=meep.Vector3(0, size[1], size[2]), weight=-1,
+                            direction=component)
+
+    force_x2 = meep.ForceRegion(center=position + meep.Vector3(size[0]/2, 0, 0),
+                            size=meep.Vector3(0, size[1], size[2]), weight=1,
+                            direction=component)
+
+    force_y1 = meep.ForceRegion(center=position - meep.Vector3(0, size[1]/2, 0),
+                            size=meep.Vector3(size[0], 0, size[2]), weight=-1,
+                            direction=component)
+
+    force_y2 = meep.ForceRegion(center=position + meep.Vector3(0, size[1]/2, 0),
+                            size=meep.Vector3(size[0], 0, size[2]), weight=1,
+                            direction=component)
+
+    force_z1 = meep.ForceRegion(center=position - meep.Vector3(0, 0, size[2]/2),
+                            size=meep.Vector3(size[0], size[1], 0), weight=-1,
+                            direction=component)
+
+    force_z2 = meep.ForceRegion(center=position + meep.Vector3(0, 0, size[2]/2),
+                            size=meep.Vector3(size[0], size[1], 0), weight=1,
+                            direction=component)
     
-    return [force_x1, force_x2, force_y1, force_y2]
+    return [force_x1, force_x2, force_y1, force_y2, force_z1, force_z2]
 
-def add_force_box_2d(sim, fcen, df, nfreq, position, size, component):
+def add_force_box(sim, fcen, df, nfreq, position, size, component):
     """add a force box to a meep simulation"""
     box = force_box(position, size, component)
     return sim.add_force(fcen, df, nfreq, *box)
@@ -65,4 +78,54 @@ def add_flux_plane(sim, fcen, df, nfreq, position, size):
 def add_flux_box(sim, fcen, df, nfreq, position, size):
     """add a flux box to a meep simulation"""
     box = flux_box(position, size)
+    return sim.add_flux(fcen, df, nfreq, *box)
+
+### 2D simulations
+
+def force_box_2d(position, size, component):
+    """create 4 force planes (2D) to calculate the component of a force"""
+    force_x1 = meep.ForceRegion(center=position - meep.Vector3(0,size[1]/2),
+                            size=meep.Vector3(size[0], 0), 
+                            direction=component, weight=-1)
+    force_x2 = meep.ForceRegion(center=position + meep.Vector3(0,size[1]/2),
+                            size=meep.Vector3(size[0], 0), 
+                            direction=component, weight=1)
+
+    force_y1 = meep.ForceRegion(center=position - meep.Vector3(size[0]/2, 0),
+                            size=meep.Vector3(0, size[1]), 
+                            direction=component, weight=-1)
+    force_y2 = meep.ForceRegion(center=position + meep.Vector3(size[0]/2, 0),
+                            size=meep.Vector3(0, size[1]), 
+                            direction=component, weight=1)
+    
+    return [force_x1, force_x2, force_y1, force_y2]
+
+def add_force_box_2d(sim, fcen, df, nfreq, position, size, component):
+    """add a force box to a meep simulation"""
+    box = force_box_2d(position, size, component)
+    return sim.add_force(fcen, df, nfreq, *box)
+    return sim.add_flux(fcen, df, nfreq, *box)
+
+def flux_box_2d(position, size):
+    """create 4 flux planes (2D) of a closed box with size and position"""
+    position = meep.Vector3(*position)
+    size = meep.Vector3(*size)
+
+    flux_x1 = meep.FluxRegion(center=position - meep.Vector3(size[0]/2, 0),
+                            size=meep.Vector3(0, size[1]), weight=-1)
+
+    flux_x2 = meep.FluxRegion(center=position + meep.Vector3(size[0]/2, 0),
+                            size=meep.Vector3(0, size[1]), weight=1)
+
+    flux_y1 = meep.FluxRegion(center=position - meep.Vector3(0, size[1]/2),
+                            size=meep.Vector3(size[0], 0), weight=-1)
+
+    flux_y2 = meep.FluxRegion(center=position + meep.Vector3(0, size[1]/2),
+                            size=meep.Vector3(size[0], 0), weight=1)
+
+    return [flux_x1, flux_x2, flux_y1, flux_y2]
+
+def add_flux_box_2d(sim, fcen, df, nfreq, position, size):
+    """add a flux box to a meep simulation"""
+    box = flux_box_2d(position, size)
     return sim.add_flux(fcen, df, nfreq, *box)
