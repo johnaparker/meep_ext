111331c29b0174fe328965e5824122cef9cd5b77
John <japarker@uchicago.edu>
2018-03-26 16:44:49 -0500

    added scalar multiple to materials

--- meep_ext/material.py
+++ meep_ext/material.py
@@ -3,28 +3,28 @@ import meep
 
 eV_um_scale = 1/1.23984193*1e6
 
-def drude_lorentz_material(freq, gamma, sigma, eps_inf=1):
+def drude_lorentz_material(freq, gamma, sigma, eps_inf=1, multiplier=1):
     """return a drude-lorentz material, where the first index is the Drude term"""
     Npoles = len(freq)
     susc = []
 
     for i in range(Npoles):
         func = meep.DrudeSusceptibility if i == 0 else meep.LorentzianSusceptibility
-        susc.append(func(frequency=freq[i], gamma=gamma[i], sigma=sigma[i]))
+        susc.append(func(frequency=freq[i], gamma=gamma[i], sigma=sigma[i]*multiplier))
 
-    material = meep.Medium(epsilon=eps_inf, E_susceptibilities=susc)
+    material = meep.Medium(epsilon=eps_inf*multiplier, E_susceptibilities=susc)
     return material
 
-def lorentz_material(freq, gamma, sig,a, eps_inf=1):
+def lorentz_material(freq, gamma, sig,a, eps_inf=1, multiplier=1):
     """return a lorentz material"""
 
     freq, gamma, sigma = map(np.asarray, [freq, gamma, sigma])
 
     Npoles = len(freq)
     func = meep.LorentzianSusceptibility
-    susc = [func(frequency=freq[i], gamma=gamma[i], sigma=sigma[i]) for i in range(Npoles)]
+    susc = [func(frequency=freq[i], gamma=gamma[i], sigma=sigma[i]*multiplier) for i in range(Npoles)]
 
-    material = meep.Medium(epsilon=eps_inf, E_susceptibilities=susc)
+    material = meep.Medium(epsilon=eps_inf*multiplier, E_susceptibilities=susc)
     return material
 
 def fit_drude_lorentz(eps, wavelength):
@@ -56,7 +56,7 @@ def get_eps(material):
 
     return eps
 
-def Au():
+def Au(multiplier=1):
     """Gold material"""
     wp  = 9.01*eV_um_scale
     f   = eV_um_scale*np.array([1e-20, 4.25692])
@@ -64,4 +64,4 @@ def Au():
     sig = wp**2/f**2*np.array([0.970928, 1.2306])
     eps_inf = 3.63869
 
-    return drude_lorentz_material(f, gam, sig, eps_inf)
+    return drude_lorentz_material(f, gam, sig, eps_inf, multiplier=multiplier)
